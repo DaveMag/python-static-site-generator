7f7a94b8384bb4fb94810c486d34a502dca127ca
SMD8 <davidemao86@gmail.com>
2022-09-24 19:20:25 +0200

    Correct Module1

--- ssg.py
+++ ssg.py
@@ -1,10 +1,10 @@
 from ssg.site import Site
 import typer
 
+
 def main(source="content", dest="dist"):
-    config=dict({source:"source", dest:"dest"})
-    Site(config.source, config.dest).build() 
-    
-    
-    
-typer.run(main)       
+    config = {"source": source, "dest": dest}
+    Site(**config).build()
+
+
+typer.run(main)

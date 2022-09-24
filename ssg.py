3e9adfe306d674cf9e9aada941945169fe2aa4ab
SMD8 <davidemao86@gmail.com>
2022-09-24 20:56:31 +0200

    Commit for module 2

--- ssg.py
+++ ssg.py
@@ -1,9 +1,13 @@
 from ssg.site import Site
+import ssg.parsers
 import typer
 
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {"source": source,
+              "dest": dest,
+              "parsers": [ssg.parsers.ResourceParser()]
+              }
     Site(**config).build()
 
 

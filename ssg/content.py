f667d4a666741c909bfe8e3e2585dfd616b8d1f9
SMD8 <davidemao86@gmail.com>
2022-09-25 13:30:04 +0200

    fixing

--- ssg/content.py
+++ ssg/content.py
@@ -3,14 +3,14 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
     
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader= FullLoader)
-        cls(metadata, content)
+        return cls(metadata, content)
         
     def __init__(self, metadata, content):
         self.data = metadata
@@ -29,7 +29,7 @@ class Content(Mapping):
         self.data["type"] = type
         
     def __getitem__(self, key):
-        self.data[key]
+        return self.data[key]
         
     def __iter__(self):
         self.data.__iter__()
